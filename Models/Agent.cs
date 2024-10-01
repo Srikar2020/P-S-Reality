cafd6cae4e041cf3c7bf28125afb37209bd51e1a
Srikar Kudumula <68241974+Srikar2020@users.noreply.github.com>
2024-10-01 09:09:58 +0530

    Added to class Agent

--- Models/Agent.cs
+++ Models/Agent.cs
@@ -1,7 +1,43 @@
-﻿namespace P_S_Reality.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P_S_Reality.Models
 {
     public class Agent
     {
-        
+        public int AgentID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Range(0,50)]
+        public int ExperienceYears { get; set; }
+
+
+        [StringLength(100)]
+        public string Specialization { get; set; }
+
+        [Range(0 ,5)]
+        public double AverageRating { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateJoined { get; set; }
+
+        public int GetTotalPropertiesListed()
+        {
+            return GetTotalPropertiesListed?.Count ?? 0;
+        }
+
+        public ICollection<Property> Properties { get; set; } = new List<Property>();
+
+
     }
 }
