4933658afdc3e45135f3c16bfc86b9a05f69cfdd
Srikar Kudumula <68241974+Srikar2020@users.noreply.github.com>
2024-10-01 11:13:07 +0530

    Added to the Neighborhood Class

--- Models/BuyerSeller.cs
+++ Models/BuyerSeller.cs
@@ -1,6 +1,38 @@
-﻿namespace P_S_Reality.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P_S_Reality.Models
 {
     public class BuyerSeller
     {
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        public string PreferredContactMethod { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateRegistered { get; set; }
+
+        public int GetTotalListedProperties()
+        {
+            return GetTotalListedProperties?.Count ?? 0;
+        }
+
+        public ICollection<Property> ListedProperties { get; set; } = new List<Property>();
+
+        public ICollection<Property> InterestedProperties { get; set; } = new List<Property>();
     }
 }
--- Models/Neighborhood.cs
+++ Models/Neighborhood.cs
@@ -1,6 +1,43 @@
-﻿namespace P_S_Reality.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P_S_Reality.Models
 {
     public class Neighborhood
     {
+        public int LocationID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string State { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string ZipCode { get; set; }
+
+        [Range (0, 100000000)]
+        public decimal AveragePrice { get; set; }
+
+        [Range (0, 10)]
+        public double PopularityScore { get; set; }
+        public int Population { get; set; }
+        public string CrimeRate { get; set; }
+
+        public ICollection<Property> Properties { get; set; } = new List<Property>();
+
+        public int GetTotalProperties()
+        {
+            return Properties?.Count ?? 0;
+        }
+
+
+        
     }
 }
