4ce780bbca6df2a8b52e7bd3f8b4e3a9688ef099
Cassandra5863 <93055245+Cassandra5863@users.noreply.github.com>
2024-10-03 19:47:11 -0400

    Adding Display Names

--- Models/BuyerSeller.cs
+++ Models/BuyerSeller.cs
@@ -4,26 +4,32 @@ namespace P_S_Reality.Models
 {
     public class BuyerSeller
     {
+        [DisplayName("Buyer/Seller ID")]
         public int BuyerSellerID { get; set; }
 
         [Required]
         [StringLength(100)]
+        [DisplayName("Full Name")]
         public string? FullName { get; set; }
 
         [Required]
         [EmailAddress]
+        [DisplayName("Email Address")]
         public string? EmailAddress { get; set; }
 
         [Required]
         [Phone]
+        [DisplayName("Phone Number")]
         public string? PhoneNumber { get; set; }
 
         [Required]
         public string? Role { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayName("Date Registered")]
         public DateTime? DateRegistered { get; set; }
 
+        [DisplayName("Preferred Contact Method")]
         public string? PreferredContactMethod { get; set; }
 
 
@@ -32,3 +38,4 @@ namespace P_S_Reality.Models
         public ICollection<Property> ListedProperties { get; set; } = new List<Property>();
     }
 }
+
