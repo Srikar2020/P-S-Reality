860ad433f365fe89eee55bf4059ec452cd54c29d
Cassandra5863 <93055245+Cassandra5863@users.noreply.github.com>
2024-10-03 19:48:40 -0400

    Adding Display Names

--- Models/Property.cs
+++ Models/Property.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,8 +7,10 @@ namespace P_S_Reality.Models
 {
     public class Property
     {
+        [DisplayName("Property ID")]
         public int PropertyID { get; set; }
 
+        [DisplayName("Seller ID")]
         public int SellerID { get; set; }
         [ForeignKey("SellerID")]
         public BuyerSeller? Seller { get; set; }
@@ -17,7 +20,6 @@ namespace P_S_Reality.Models
         [ForeignKey("BuyerID")]
         public BuyerSeller? Buyer {  get; set; }
 
-
         [Required]
         [StringLength(200)]
         public string? Address { get; set; }
@@ -26,9 +28,11 @@ namespace P_S_Reality.Models
         [Range(50000, 10000000)]
         public decimal Price { get; set; }
 
+        [DisplayName("Neighborhood ID")]
         public int NeighborhoodID { get; set; }
         public Neighborhood? Neighborhood { get; set; }
 
+        [DisplayName("Agent ID")]
         public int AgentID { get; set; }
         public Agent? Agent { get; set; }
 
@@ -47,17 +51,23 @@ namespace P_S_Reality.Models
 
 
         [Range(100, 20000)]
+        [DisplayName("Square Footage")]
         public int SquareFootage { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayName("Listed Date")]
         public DateTime ListedDate { get; set; }
 
         public string? Status { get; set; }
 
 
+        [DisplayName("Has Garage")]
         public bool HasGarage { get; set; }
+
+        [DisplayName("Has Garden")]
         public bool HasGarden { get; set; }
 
+        [DisplayName("Is Furnished")]
         public bool IsFurnished { get; set; }
 
 
