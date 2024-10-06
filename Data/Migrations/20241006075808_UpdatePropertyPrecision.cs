487b985c98de9258e5d91b0f18dc5c93745218e6
Srikar Kudumula <68241974+Srikar2020@users.noreply.github.com>
2024-10-06 13:42:37 +0530

    Migrations after pulling

---
+++ Data/Migrations/20241006075808_UpdatePropertyPrecision.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P_S_Reality.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePropertyPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
