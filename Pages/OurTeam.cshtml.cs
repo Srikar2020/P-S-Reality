f39ed0e6e745043a55e6c771d22265757196d81e
Srikar Kudumula <68241974+Srikar2020@users.noreply.github.com>
2024-10-10 13:08:22 +0530

    Added OurTeam page to the website and aded some styling in site.css

---
+++ Pages/OurTeam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace P_S_Reality.Pages
+{
+    public class OurTeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
