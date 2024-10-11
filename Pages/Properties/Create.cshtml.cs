a3738d974918b1f2b1a389c0c0d25a408db2bcb5
Cassandra5863 <93055245+Cassandra5863@users.noreply.github.com>
2024-10-10 21:06:29 -0400

    Update Create.cshtml.cs

--- Pages/Properties/Create.cshtml.cs
+++ Pages/Properties/Create.cshtml.cs
@@ -11,6 +11,7 @@ using P_S_Reality.Models;
 
 namespace P_S_Reality.Pages.Properties
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly P_S_Reality.Data.ApplicationDbContext _context;
